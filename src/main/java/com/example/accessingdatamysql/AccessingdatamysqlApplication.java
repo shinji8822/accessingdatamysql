3c2ca1b7199d0b84f98a9b4c5cc288c2a7d4136f
shinji <shinji@foliog1.local>
2023-01-29 21:33:54 +0900

    20230129_1

---
+++ src/main/java/com/example/accessingdatamysql/AccessingdatamysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingdatamysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingdatamysqlApplication.class, args);
+	}
+
+}
